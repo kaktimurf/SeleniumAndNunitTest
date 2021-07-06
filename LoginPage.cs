ec0c483fec7fe99c12ee2367aa4d84a42e2c4f0a
Ferit Kaktimur <kaktimurf@gmail.com>
2021-07-06 10:52:50 +0300

    Complete

---
+++ LoginPage.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumLearn
+{
+    class LoginPage
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void BrowserOpen()
+        {
+            driver = new ChromeDriver("C:\\Program Files\\Google\\Chrome\\Application");
+            driver.Manage().Window.Maximize();
+        }
+        
+        [Test]
+        public void Login()
+        {
+            driver.Url = "https://ogrposta.selcuk.edu.tr/";
+            Thread.Sleep(4000);
+            IWebElement element = driver.FindElement(By.XPath("//*[@id='username']"));
+            element.SendKeys("ogrNo@ogr.selcuk.edu.tr");//öğrenci epostası
+            element = driver.FindElement(By.XPath("//*[@id='password']"));
+            element.SendKeys("ogrPassword");//öğrenci parolası
+            element = driver.FindElement(By.XPath("//*[@type='submit' and contains(@class,DwtButton)]"));
+            element.Click();
+            driver.Manage().Timeouts();
+            Thread.Sleep(10000);
+            Thread.Sleep(5000);
+
+        }
+        [TearDown]
+        public void Close()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniunAndNUnitLearnDemo.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumLearn
+{
+    class SeleniunAndNUnitLearnDemo
+    {
+       // IWebDriver driver;
+
+        //[SetUp]
+        //public void startBrowser()
+        //{
+        //    driver = new ChromeDriver("C:\\Program Files\\Google\\Chrome\\Application");
+        //}
+
+        //[Test]
+        
+        //public void test()
+        //{
+        //    driver.Url = "http://www.google.co.in";
+        //}
+
+        //[TearDown]
+        //public void closeBrowser()
+        //{
+        //    driver.Close();
+        //}
+    }
+}
